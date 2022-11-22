1055e153abd7c7a66de7ae7b9dc169ac3d651cf9
Christian Wach <needle@haystack.co.uk>
2022-11-22 16:50:50 +0000

    Update activity sidebar

--- assets/templates/activity_sidebar.php
+++ assets/templates/activity_sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Activity Sidebar template.
+ * Activity Sidebar Template.
  *
  * @package CommentPress_Poets
  */
@@ -22,18 +22,18 @@ if ( $_is_commentable && ! post_password_required() ) {
 
 	// Get singular Post Type label.
 	$current_type = get_post_type();
-	$current_post_type = get_post_type_object( $current_type );
+	$post_type_obj = get_post_type_object( $current_type );
 
 	/**
-	 * Assign name of post type.
+	 * Filters the name of the Post Type.
 	 *
 	 * @since 3.8.10
 	 *
-	 * @param str $singular_name The singular label for this post type
-	 * @param str $current_type The post type identifier
-	 * @return str $singular_name The modified label for this post type
+	 * @param str $singular_name The singular label for this Post Type.
+	 * @param str $current_type The Post Type identifier.
+	 * @return str $singular_name The modified label for this Post Type.
 	 */
-	$post_type_name = apply_filters( 'commentpress_lexia_post_type_name', $current_post_type->labels->singular_name, $current_type );
+	$post_type_name = apply_filters( 'commentpress_lexia_post_type_name', $post_type_obj->labels->singular_name, $current_type );
 
 	// Construct recent comments phrase.
 	$_paragraph_text = sprintf(
@@ -42,13 +42,16 @@ if ( $_is_commentable && ! post_password_required() ) {
 		$post_type_name
 	);
 
-	// Set default.
-	$page_comments_title = apply_filters(
-		'cp_activity_tab_recent_title_page',
-		$_paragraph_text
-	);
+	/**
+	 * Filters the "Recent Comments" phrase.
+	 *
+	 * @since 3.4
+	 *
+	 * @param str $_paragraph_text The default "Recent Comments" phrase.
+	 */
+	$page_comments_title = apply_filters( 'cp_activity_tab_recent_title_page', $_paragraph_text );
 
-	// Get page comments.
+	// Get Page Comments.
 	$_page_comments_output = commentpress_get_comment_activity( 'post' );
 
 }
@@ -56,10 +59,11 @@ if ( $_is_commentable && ! post_password_required() ) {
 // Set default.
 $_all_comments_title = __( 'Latest Comments', 'commentpress-poets' );
 
-// Get all comments.
+// Get all Comments.
 $_all_comments_output = commentpress_get_comment_activity( 'all' );
 
-// Set maximum number to show - put into option?
+// Set maximum number to show.
+// TODO: Make this an option?
 $_max_members = 10;
 
 ?>
@@ -71,26 +75,39 @@ $_max_members = 10;
 	</div>
 
 	<div class="sidebar_minimiser">
-
 		<div class="sidebar_contents_wrapper">
 
 			<div class="comments_container">
 
 				<?php
 
-				// Allow widgets to be placed above activity.
+				// Allow Widgets to be placed above Activity.
 				dynamic_sidebar( 'cp-activity-top' );
 
-				// Allow plugins to add their own activity headings here.
+				/**
+				 * Allow plugins to add their own Activity Headings here.
+				 *
+				 * @since 3.4.8
+				 */
 				do_action( 'commentpress_bp_activity_sidebar_before_activity' );
 
-				// Show page comments if we can.
-				if ( $_is_commentable && $_page_comments_output != '' ) {
+				?>
 
-					// Allow plugins to add their own activity heading here.
+				<?php if ( $_is_commentable && $_page_comments_output != '' ) : ?>
+
+					<?php
+
+					// Show Page Comments if we can.
+
+					/**
+					 * Allow plugins to add their own Activity Heading here.
+					 *
+					 * @since 3.4.8
+					 */
 					do_action( 'commentpress_bp_activity_sidebar_before_page_comments' );
 
 					?>
+
 					<h3 class="activity_heading"><?php echo $page_comments_title; ?></h3>
 
 					<div class="paragraph_wrapper page_comments_output">
@@ -99,18 +116,32 @@ $_max_members = 10;
 
 					<?php
 
-					// Allow plugins to add their own activity heading here.
+					/**
+					 * Allow plugins to add their own Activity Heading here.
+					 *
+					 * @since 3.4.8
+					 */
 					do_action( 'commentpress_bp_activity_sidebar_after_page_comments' );
 
-				} // End commentable post/page check.
+					?>
 
-				// Show all comments from site if we can.
-				if ( $_all_comments_output != '' ) {
+				<?php endif; /* End commentable Post/Page check. */ ?>
 
-					// Allow plugins to add their own activity heading here.
+				<?php if ( $_all_comments_output != '' ) : ?>
+
+					<?php
+
+					// Show all Comments from Site if we can.
+
+					/**
+					 * Allow plugins to add their own Activity Heading here.
+					 *
+					 * @since 3.4.8
+					 */
 					do_action( 'commentpress_bp_activity_sidebar_before_all_comments' );
 
 					?>
+
 					<h3 class="activity_heading"><?php echo $_all_comments_title; ?></h3>
 
 					<div class="paragraph_wrapper all_comments_output start_open">
@@ -119,23 +150,33 @@ $_max_members = 10;
 
 					<?php
 
-					// Allow plugins to add their own activity heading here.
+					/**
+					 * Allow plugins to add their own Activity Heading here.
+					 *
+					 * @since 3.4.8
+					 */
 					do_action( 'commentpress_bp_activity_sidebar_after_all_comments' );
 
-				} // End comments from site check.
+					?>
 
-				// Allow plugins to add their own activity headings here.
+				<?php endif; /* End comments from site check. */ ?>
+
+				<?php
+
+				/**
+				 * Allow plugins to add their own Activity Headings here.
+				 *
+				 * @since 3.4.8
+				 */
 				do_action( 'commentpress_bp_activity_sidebar_after_activity' );
 
-				// Allow widgets to be placed below activity.
+				// Allow Widgets to be placed below Activity.
 				dynamic_sidebar( 'cp-activity-bottom' );
 
 				?>
 
 			</div><!-- /comments_container -->
-
 		</div><!-- /sidebar_contents_wrapper -->
 
 	</div><!-- /sidebar_minimiser -->
-
 </div><!-- /activity_sidebar -->
